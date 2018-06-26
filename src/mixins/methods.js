76a789e4d5fcdbe6211272cd2c224c6149a49765
Larry Clyman <lclyman@gmail.com>
2018-06-26 11:18:46 -0600

    added print to pdf

--- src/mixins/methods.js
+++ src/mixins/methods.js
@@ -8,7 +8,54 @@ const methods = {
         this.$store.commit('showNavbar')
         this.txtPrintable = 'Printable version'
       }
-    }
+    },
+    getData() {
+      let arrItems = []
+      if(this.productsDue) {
+        this.productsDue.forEach(el => {
+          arrItems.push({
+            BrandName: el.brandName,
+            ProductName: el.receiptAlias,
+            Scancode: el.scancode,
+            SellbyDate: el.SellbyDate
+          })
+        })
+      } else {
+        this.reorders.forEach(el => {
+          arrItems.push({
+            BrandName: el.brandName,
+            ProductName: el.receiptAlias,
+            Scancode: el.scancode,
+          })
+        })
+      }
+
+      return arrItems
+    },
+    getColumns() {
+      return [{
+          title: "Brand Name",
+          dataKey: "BrandName"
+        },
+        {
+          title: "Product name",
+          dataKey: "ProductName"
+        },
+        {
+          title: "Scancode",
+          dataKey: "Scancode"
+        },
+        {
+          title: "Sell By Date",
+          dataKey: "SellbyDate"
+        }
+      ]
+    },
+    exportPdf() {
+      let doc = new jsPDF("p", "pt")
+      doc.autoTable(this.getColumns(), this.getData())
+      doc.save("SellByDue.pdf")
+    },
   }
 }
 
